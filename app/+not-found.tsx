b2706b2e74a3f0fe6e8fec31719ec27e45e814df
Bastienne-Banco Théo <theo.bastienne-banco@etu.univ-lyon1.fr>
2025-02-27 18:16:26 +0100

    test de commit

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -5,6 +5,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
+  //s
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
